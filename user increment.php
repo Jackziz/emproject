2dd04cd2d7045d6d52361ac0c43fbca0d85bf818
Jackziz <evgchapl@gmail.com>
2016-01-03 00:27:33 +0200

    Счетчик юзеров для прикола сделал) Он понадобится, да и на MVC там нечего разделять...

--- user increment.php
+++ user increment.php
@@ -9,7 +9,7 @@ $file = fopen("data.txt", "a+"); //Open file and set priorities to read and addi
 flock($file,LOCK_EX);//file locked
 $count = fread($file,100);//read txt file
 $count++;// + number in txt file
-$fruncate($file,0);//clean the file with previous number
+ftruncate($file,0);//clean the file with previous number
 fwrite($file,$count);//write the number++1
 flock($file,LOCK_UN);// Unlock the file(because we want to write in it)
 fclose($file);// Lock file. So, we clean the memory and work more)
