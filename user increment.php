79c0586a5cbae9c8e8e714d4afb69b86d04a5802
Jackziz <evgchapl@gmail.com>
2016-01-03 00:15:46 +0200

    Счетчик юзеров для прикола сделал) Он понадобится, да и на MVC там нечего разделять...

---
+++ user increment.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: USER
+ * Date: 03.01.16
+ * Time: 0:13
+ */
+$file = fopen("data.txt", "a+"); //Open file and set priorities to read and addind data to the end of file.
+flock($file,LOCK_EX);//file locked
+$count = fread($file,100);//read txt file
+$count++;// + number in txt file
+$fruncate($file,0);//clean the file with previous number
+fwrite($file,$count);//write the number++1
+flock($file,LOCK_UN);// Unlock the file(because we want to write in it)
+fclose($file);// Lock file. So, we clean the memory and work more)
+echo "Our site has been visites by $count users.";
